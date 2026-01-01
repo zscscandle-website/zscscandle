319d2cbad3832a2659273c8348284e444a206970
zscscandle-website <dignercsabi@gmail.com>
2026-01-01 23:12:44 +0100

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="hu">
+      <head>
+        <title>Zs&CsCandle</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
